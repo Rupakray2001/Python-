24d37f126439fcf48a7a61ee2f641e9f2cd63e02
Rupakray2001 <62040877+Rupakray2001@users.noreply.github.com>
2020-03-14 21:29:16 +0530

    Add files via upload

---
+++ Data Type Conversion Table.py
@@ -0,0 +1,73 @@
+#String to Bool
+a="Hello"
+b=bool(a)
+print(type(a))
+print(type(b))
+
+#String to Int "ValueError: invalid literal for int() with base 10: 'Hello'"
+'''a="Hello"
+b=int(a)
+print(type(a))
+print(type(b))'''
+
+#String to Float "ValueError: could not convert string to float: 'Hello'"
+'''a="Hello"
+b=float(a)
+print(type(a))
+print(type(b))'''
+
+
+#Bool to String
+a=True
+b=str(a)
+print(type(a))
+print(type(b))
+
+#Bool to int
+a=True
+b=int(a)
+print(type(a))
+print(type(b))
+
+#Bool to float
+a=True
+b=float(a)
+print(type(a))
+print(type(b))
+
+#Int to String
+a=5
+b=str(a)
+print(type(a))
+print(type(b))
+
+#Int to Bool
+a=5
+b=bool(a)
+print(type(a))
+print(type(b))
+
+#Int to Float
+a=5
+b=float(a)
+print(type(a))
+print(type(b))
+
+#Float to String
+a=5.0
+b=str(a)
+print(type(a))
+print(type(b))
+
+#Float to Bool
+a=5.0
+b=bool(a)
+print(type(a))
+print(type(b))
+
+#Float to Int
+a=5.0
+b=int(a)
+print(type(a))
+print(type(b))
+
---
+++ Geometry.py
@@ -0,0 +1,28 @@
+r=9.2
+h=3
+pi=22/7
+volume=(1/3)*pi*(r**2)*h
+area=int(volume)
+print("volume of the cone=",area)
+
+n=5
+p=5
+m=5*12
+r=13
+s=13/100
+t=(n*m*s)
+u=float(t)
+print("output of npr=",t)
+print(type(u))
+
+r=3.2
+pi=22/7
+area=(22/7)*(r**2)
+print("Area of the circle is", area)
+print(type(area))
+
+r=3.8
+pi=22/7
+circum=2*(22/7)*r
+print("Circumference of the circle is", circum)
+print(type(circum))
