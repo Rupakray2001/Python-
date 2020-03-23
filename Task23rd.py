e067f69fbb9075a2656a9b7cdb18e5a6747519e2
Rupakray2001 <62040877+Rupakray2001@users.noreply.github.com>
2020-03-23 23:08:07 +0530

    Add files via upload

---
+++ Task23rd.py
@@ -0,0 +1,14 @@
+#task1
+a=input("provide one string")
+b=input("provide another string")
+print(len(a+b))
+print(a[0]+a[-1]+b[0]+b[-1])
+
+
+#task2
+e=input("Please provide a string")
+f=e[0]
+g=f.upper()
+h=e[-1]
+i=h.upper()
+print(g+e[1:-1]+i)
