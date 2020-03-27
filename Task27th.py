bc2b3a59f4af4f0de9c7a66b6ae76d6afe73ade3
Rupakray2001 <62040877+Rupakray2001@users.noreply.github.com>
2020-03-27 22:35:26 +0530

    Add files via upload

---
+++ Task27th.py
@@ -0,0 +1,40 @@
+#input is "computer_science"
+a="computer_science"
+#providing empty value since it starts from 2 and ends at 2 
+print(a[2:2:2])
+#output would be empty value
+print(a[2:-2:-2])
+#output would be empty value
+print(a[-2:2:2])
+#output would be cec_eu
+print(a[-2:2:-2])
+#output would be ecneics_re
+print(a[:5:-1])
+#output would be computer_sc
+print(a[:-5:1])
+#output would be ecne
+print(a[:-5:-1])
+#output would be cience
+print(a[10::1]) 
+#output would be cs_retupmoc
+print(a[10::-1])
+#output would be c_eumc
+print(a[10::-2]) 
+#output would be ctc 
+print(a[10::-5])
+#output would be e
+print(a[-10:10:10])
+#output would be cc
+print(a[::10])
+#output would be ectc
+print(a[::-5])
+#output would be er
+print(a[::-8])
+#output would be cmue
+print(a[:-8:2])
+#output would be blank
+print(a[-5:5:2])
+#output would be blank
+print(a[-10:1:1])
+#output would be blank
+print(a[-0:9:-2])
