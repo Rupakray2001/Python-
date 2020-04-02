407089d53025a78f6810470f4c18e590a3960610
Rupakray2001 <62040877+Rupakray2001@users.noreply.github.com>
2020-04-02 08:08:49 +0530

    Add files via upload

---
+++ tast3rdApr.py
@@ -0,0 +1,11 @@
+list1=[[1,2,3],4,5,6,[8,9],[[44,55,66],[66,77],["hello","python","welcome"]]]
+#for getting welcome
+print(list1[-1][-1][-1])
+#for getting come
+print(list1[-1][-1][-1][3:])
+#for getting yth
+print(list1[-1][-1][-2][1:4])
+#for getting llo
+print(list1[-1][-1][-3][2:])
+#for getting "python","welcome"
+print(list1[-1][-1][1:])
