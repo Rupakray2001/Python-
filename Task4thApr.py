a4f2c0e2fff4d907f666c60bf0e11134ac879c25
Rupakray2001 <62040877+Rupakray2001@users.noreply.github.com>
2020-04-03 07:48:24 +0530

    Add files via upload

---
+++ Task4thApr.py
@@ -0,0 +1,31 @@
+#Task 1
+#1 convert list to tuple
+a=[3,4,5,6,7,8]
+b=tuple(a)
+print(type(a))
+print(type(b))
+
+#2 convert tuple to list
+c=(3,4,5,6,7,8)
+d=list(c)
+print(type(d))
+print(type(c))
+
+#task 2
+
+
+#index
+
+#count
+e=(3,4,5,6,7,8,3,3,8)
+print(e.count(8))
+
+#index
+g=(3,4,5,6,7,8,3,3,8)
+print(a[3])
+
+#del - 'tuple' object doesn't support item deletion
+'''f=(3,4,5,6,7,8,3,3,8)
+del f[3]
+print(f)'''
+
